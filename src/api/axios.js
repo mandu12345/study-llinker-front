9c5d502f9c379562e99307ed0d0d5b2ed68556ae
mandu12345 <jisu030309@naver.com>
2025-09-27 22:58:26 +0900

    도메인 주소 오류 수정

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api", // 👉 백엔드 API 주소로 변경
+  baseURL:
+    window.location.hostname === "localhost"
+      ? "http://localhost:8080/api"     // 로컬 개발용
+      : "http://gachon.studylink.click/api", // 배포용
 });
 
-// 요청 시 JWT 토큰 자동 포함
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
